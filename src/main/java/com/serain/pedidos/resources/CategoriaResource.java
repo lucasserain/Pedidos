d2d8ee029cfc5efd02ccc38fbb3b08caa1405845
Lucas Serain <lucas.serain@outlook.com>
2020-05-03 00:25:36 -0300

    Criacao de Categoria Domain e exibicao em Resources.

--- src/main/java/com/serain/pedidos/resources/CategoriaResource.java
+++ src/main/java/com/serain/pedidos/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package com.serain.pedidos.resources;
 
+import com.serain.pedidos.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar(){
-        return "Rest está funcionando.";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1,"Informática");
+        Categoria cat2 = new Categoria(2,"Escritório");
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.add(cat1);
+        categorias.add(cat2);
+        return categorias;
     }
 }
