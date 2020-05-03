2a16e99064a4ad989256734772b365c6ee5e9152
Lucas Serain <lucas.serain@outlook.com>
2020-05-02 21:08:24 -0300

    first commit

---
+++ src/main/java/com/serain/pedidos/PedidosApplication.java
@@ -0,0 +1,14 @@
+package com.serain.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosApplication.class, args);
+		System.out.println("tESTE");
+	}
+
+}
